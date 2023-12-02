5c3be573b1019489e47a6bd039737f28e62de646
Maël Obréjan <mael.obrejan@protonmail.com>
2023-12-02 14:14:19 +0100

    chore(day1/part2): cleanup code
    Make the code more idiomatic and remove unnecessary allocations.

--- src/bin/day1part2.rs
+++ src/bin/day1part2.rs
@@ -11,23 +11,27 @@ fn main() -> Result<(), anyhow::Error> {
         }
 
         let mut index = 0usize;
-
-        let mut first = None;
-        while first.is_none() {
+        let first;
+        loop {
             let buffer = &line[index..];
 
-            first = buffer
-                .starts_with(|c: char| c.is_ascii_digit())
-                .then_some(buffer.chars().next().expect("starts with a digit"))
-                .or_else(|| buffer.starts_with("one").then_some('1'))
-                .or_else(|| buffer.starts_with("two").then_some('2'))
-                .or_else(|| buffer.starts_with("three").then_some('3'))
-                .or_else(|| buffer.starts_with("four").then_some('4'))
-                .or_else(|| buffer.starts_with("five").then_some('5'))
-                .or_else(|| buffer.starts_with("six").then_some('6'))
-                .or_else(|| buffer.starts_with("seven").then_some('7'))
-                .or_else(|| buffer.starts_with("eight").then_some('8'))
-                .or_else(|| buffer.starts_with("nine").then_some('9'));
+            if let Some(digit) = buffer
+                .chars()
+                .next()
+                .and_then(|c| c.to_digit(10))
+                .or_else(|| buffer.starts_with("one").then_some(1))
+                .or_else(|| buffer.starts_with("two").then_some(2))
+                .or_else(|| buffer.starts_with("three").then_some(3))
+                .or_else(|| buffer.starts_with("four").then_some(4))
+                .or_else(|| buffer.starts_with("five").then_some(5))
+                .or_else(|| buffer.starts_with("six").then_some(6))
+                .or_else(|| buffer.starts_with("seven").then_some(7))
+                .or_else(|| buffer.starts_with("eight").then_some(8))
+                .or_else(|| buffer.starts_with("nine").then_some(9))
+            {
+                first = digit;
+                break;
+            }
 
             if index > line.len() {
                 bail!("line has no digit: {line}");
@@ -37,29 +41,34 @@ fn main() -> Result<(), anyhow::Error> {
         }
 
         let mut index = line.len();
-        let mut last = None;
-        while last.is_none() {
+        let last;
+        loop {
             let buffer = &line[..index];
 
-            last = buffer
-                .ends_with(|c: char| c.is_ascii_digit())
-                .then_some(buffer.chars().last().expect("ends_with returned true"))
-                .or_else(|| buffer.ends_with("one").then_some('1'))
-                .or_else(|| buffer.ends_with("two").then_some('2'))
-                .or_else(|| buffer.ends_with("three").then_some('3'))
-                .or_else(|| buffer.ends_with("four").then_some('4'))
-                .or_else(|| buffer.ends_with("five").then_some('5'))
-                .or_else(|| buffer.ends_with("six").then_some('6'))
-                .or_else(|| buffer.ends_with("seven").then_some('7'))
-                .or_else(|| buffer.ends_with("eight").then_some('8'))
-                .or_else(|| buffer.ends_with("nine").then_some('9'));
+            if let Some(digit) = buffer
+                .chars()
+                .last()
+                .and_then(|c| c.to_digit(10))
+                .or_else(|| buffer.ends_with("one").then_some(1))
+                .or_else(|| buffer.ends_with("two").then_some(2))
+                .or_else(|| buffer.ends_with("three").then_some(3))
+                .or_else(|| buffer.ends_with("four").then_some(4))
+                .or_else(|| buffer.ends_with("five").then_some(5))
+                .or_else(|| buffer.ends_with("six").then_some(6))
+                .or_else(|| buffer.ends_with("seven").then_some(7))
+                .or_else(|| buffer.ends_with("eight").then_some(8))
+                .or_else(|| buffer.ends_with("nine").then_some(9))
+            {
+                last = digit;
+                break;
+            }
 
             index -= 1;
         }
 
-        let number = format!("{}{}", first.unwrap(), last.unwrap()).parse::<u8>()?;
+        let number = first * 10 + last;
 
-        Ok(sum + u16::from(number))
+        Ok(sum + number)
     })?;
 
     println!("Calibration values sum: {sum}");
